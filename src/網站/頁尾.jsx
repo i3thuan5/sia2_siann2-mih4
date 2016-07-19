e244f9e32ff4129de3a461f4bd417a677a758ff9
薛丞宏 <ihcaoe@gmail.com>
2016-07-19 13:20:57 +0800

    加下底的連結

--- src/網站/頁尾.jsx
+++ src/網站/頁尾.jsx
@@ -11,11 +11,20 @@ export default class ToLam extends React.Component {
             <a href='https://g0v.hackpad.com/f4rSgcFTIzz'>我想幫忙</a>
           </li>
           <li className='item'>
-            <a href='http://sih4sing5hong5.github.io/tai5-uan5_gian5-gi2_hok8-bu7/%E5%8C%AF%E5%85%A5%E8%B3%87%E6%96%99.html'>語料來源</a>
+            <a href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
           </li>
           <li className='item'>
-            <a href='http://docs.tai5uan5gian5gi2hok8bu7.apiary.io/'>服務API</a>
-        </li>
+            <a href='https://docs.tai5uan5gian5gi2hok8bu7.apiary.io/'>服務API</a>
+          </li>
+          <li className='item'>
+            <a href='https://github.com/sih4sing5hong5/sia2_siann2-mih4'>前端網站程式碼</a>
+          </li>
+          <li className='item'>
+            <a href='https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki'>後端服務程式碼</a>
+          </li>
+          <li className='item'>
+            <a href='https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki/Taiwanese-Corpus%E8%AA%9E%E6%96%99'>語料來源</a>
+          </li>
         </ul>
       </footer>
     );
